add650e6225016fed80a071939c3368aa8612319
Paltsevalexandr <paltsevaleksandr21@gmail.com>
2021-04-01 15:13:15 +0300

    sorting implemented, add method which add contact to favourites

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-/*list: {
-        {'name': 'Aleksandr', 'company': 'Hoolie Inc.', 'isFavourite': false, 'image': 'face1.jpg'},
-    {'name': 'John', 'company': 'US Berkeley', 'isFavourite': false, 'image': 'face2.jpg'},
-    {'name': 'Megan', 'company': 'Husky Energy', 'isFavourite': false, 'image': 'face.jpg'}
-      }*/
+
 void main() {
   runApp(MaterialApp(
         title: 'Contact list',
@@ -21,13 +17,13 @@ class ContactList extends StatefulWidget {
 class _ListState extends State<ContactList> {
   List<Map> list = [
     {'name': 'Joshua Alison', 'company': 'Hoolie Inc.', 'isFavourite': true, 'image': 'face1.jpg'},
-    {'name': 'John Agnew', 'company': 'Stanford University', 'isFavourite': true, 'image': 'face1.jpg'},
-    {'name': 'Sam Barnard', 'company': 'US Berkeley', 'isFavourite': true, 'image': 'face1.jpg'},
-    {'name': 'Aleksandr Paltsev', 'company': 'Hoolie Inc.', 'isFavourite': true, 'image': 'face1.jpg'},
-    {'name': 'Joel Cahnon', 'company': 'US Berkeley', 'isFavourite': false, 'image': 'face2.jpg'},
-    {'name': 'Kyle Dickenson', 'company': 'Pied Piper', 'isFavourite': true, 'image': 'face1.jpg'},
-    {'name': 'Lauren Davis', 'company': 'US Berkeley', 'isFavourite': true, 'image': 'face1.jpg'},
-    {'name': 'Megan Blakely', 'company': 'Husky Energy', 'isFavourite': false, 'image': 'face4.jpg'}
+    {'name': 'John Agnew', 'company': 'Stanford University', 'isFavourite': true, 'image': 'face2.jpg'},
+    {'name': 'Sam Barnard', 'company': 'US Berkeley', 'isFavourite': true, 'image': 'face3.jpg'},
+    {'name': 'Joel Cahnon', 'company': 'US Berkeley', 'isFavourite': false, 'image': 'face8.jpg'},
+    {'name': 'Kyle Dickenson', 'company': 'Pied Piper', 'isFavourite': true, 'image': 'face7.jpg'},
+    {'name': 'Lauren Davis', 'company': 'US Berkeley', 'isFavourite': true, 'image': 'face4.jpg'},
+    {'name': 'Olga Petrova', 'company': 'Husky Energy', 'isFavourite': false, 'image': 'face5.jpg'},
+    {'name': 'Megan Blakely', 'company': 'Husky Energy', 'isFavourite': false, 'image': 'face6.jpg'}
   ];
 
   Function addToFavourites(String name, String company) {
@@ -35,19 +31,36 @@ class _ListState extends State<ContactList> {
       () {
         setState(() {
           list = [
-            for(Map item in list) if(item['name'] == name && item['company'] == company) {...item, 'isFavourite': !item['isFavourite']} else item
+            for(Map item in list) 
+              if(item['name'] == name) 
+                {...item, 'isFavourite': !item['isFavourite']} 
+              else 
+                item
           ];
-          /*list = list
-                .map((item) => item['name'] == name ? {...item, 'isFavourite': true} : item)
-                .toList();*/
         });
       }
     );
   }
   List sortContacts() {
-    List sortedList = list;
-    sortedList.sort((a, b) => a > b ? 1 : 0);
+    List<Map> sortedList = List.from(list);
+    sortedList.sort((a, b) => a['name'].split(' ')[1].compareTo(b['name'].split(' ')[1]));
     return sortedList;
+  }
+  
+  List contactsView(List list) {
+    List result = [];
+    String firstLetter = '';
+    for(int i = 0; i < list.length; i++) {
+
+      if(list[i]['name'].split(' ')[1][0] != firstLetter) {
+        result.add(list[i]['name'].split(' ')[1][0]);
+        result.add(list[i]);
+        firstLetter = list[i]['name'].split(' ')[1][0];
+      } else {
+        result.add(list[i]);
+      }
+    }
+    return result;
   }
   @override
   Widget build(BuildContext context) {
@@ -55,10 +68,19 @@ class _ListState extends State<ContactList> {
       children: [
         Container(
           padding: EdgeInsets.only(left: 30),
-          child: Column(
-            children: list.map((contact) {
-              return SingleContact(contact, addToFavourites(contact['name'], contact['company']));
-            }).toList()
+          child: ListView(
+            children: [
+              for(var item in contactsView(sortContacts()))
+                if(item is Map)
+                  SingleContact(item, addToFavourites(item['name'], item['company']))
+                else
+                  Container(
+                      padding: EdgeInsets.only(left: 5),
+                      margin: EdgeInsets.only(bottom: 15),
+                      child: Text(
+                        item, 
+                        style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600, color: Colors.black45)))
+            ]
           )
         )
       ],
@@ -73,48 +95,54 @@ class SingleContact extends StatelessWidget {
   SingleContact(this.contact, this.addToFavourites);
 
   Widget build(BuildContext context) {
-    var image = contact['image'];
+    var star = 
+      Container(
+        width: 40, 
+        height: 40,
+        margin: EdgeInsets.only(right: 20),
+        child: GestureDetector(
+          child: contact['isFavourite']
+          ? Container(
+              child: Image.asset('assets/images/star.png'), 
+              padding: EdgeInsets.all(12),
+              width: 40, height: 40)   
+          : null,
+          onTap: addToFavourites),
+      );
+    var avatar = 
+      Container(
+        width: 40, height: 40,
+        margin: EdgeInsets.only(right: 20),
+        child: CircleAvatar(
+          radius: 50,
+          backgroundImage: AssetImage('assets/images/${contact['image']}'))
+      );
+    var nameAndCompany = 
+        Container(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children:[
+              Container(
+                margin: EdgeInsets.only(bottom: 5),
+                child: Text(contact['name'], 
+                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.w600, color: Colors.black))
+              ),
+              Text(
+                contact['company'], 
+                style: TextStyle(fontSize: 12, fontWeight: FontWeight.w400, color: Colors.black38))
+            ],
+          ),
+          margin: EdgeInsets.only(right: 20));
+
     return Container(
       margin: EdgeInsets.only(bottom: 10),
-      width: 500,
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          Container(
-            width: 20, 
-            height: 20,
-            child: GestureDetector(
-              child: contact['isFavourite']
-                  ? Image.asset('assets/images/star.png', width: 20, height: 20)
-                  : null,
-              onTap: addToFavourites),
-            margin: EdgeInsets.only(right: 20)
-          ),
-          Container(
-            width: 40, height: 40,
-            margin: EdgeInsets.only(right: 10),
-            child: CircleAvatar(
-              backgroundImage: AssetImage('assets/images/$image'),
-              radius: 50,
-            )
-          ),
-          Container(
-            child: Column(
-              children:[
-                Text(
-                  contact['name'], 
-                  textAlign: TextAlign.center, 
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600, color: Colors.black)),
-                Text(
-                  contact['company'], 
-                  textAlign: TextAlign.center, 
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400, color: Colors.black))
-              ],
-            ),
-            margin: EdgeInsets.only(right: 20)),
+          star,
+          avatar,
+          nameAndCompany,
         ],
       ),
     );
   }
 }
-
